a4749397eebdeb229bcb916b127d8e71204e8743
baniquedg <baniquedg@104-05-2017.bi.local>
2018-02-28 14:58:47 -0500

    Threw up on Jared's golf shoes

--- Binary Trees/src/ExpressionTree.java
+++ Binary Trees/src/ExpressionTree.java
@@ -1,4 +1,35 @@
 
 public class ExpressionTree {
 
+	ExpressionTree(){
+		super();
+	}
+	
+	public ExpressionTree buildTree(String[] postfix) {
+		
+		return new ExpressionTree();
+	}
+	
+	public int evalTree(ExpressionTree e) {
+		return 0;
+	}
+	
+	public String toPrefixNotation() {
+		
+	}
+	
+	public String toInfixNotation() {
+		
+	}
+	
+	public String toPostfixNotation() {
+		
+	}
+	
+	public int postfixEval() {
+		
+	}
+	
+	
+	
 }
