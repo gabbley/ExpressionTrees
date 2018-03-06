fd407d9d5b964c6b0b35ac664f4decaa622b9caa
baniquedg <baniquedg@104-18-2017.bi.local>
2018-03-06 10:39:42 -0500

    Thoughts and prayers go to those 4 students in DE CS II

--- Binary Trees/src/ExpressionTree.java
+++ Binary Trees/src/ExpressionTree.java
@@ -9,26 +9,31 @@ public class ExpressionTree {
 		super();
 	}
 
-	public ExpressionTree buildTree(String[] postfix) {
+	private TreeNode buildTree(String[] postfix) {
 
-		Stack<Object> exp = new Stack<Object>();
+		Stack<TreeNode> exp = new Stack<TreeNode>();
+		ExpressionTree tree = new ExpressionTree();
 		for (int i = 0; i < postfix.length; i++) {
-			if (isOperator(postfix[i])) {
-				exp.push(postfix[i]);
+			if (!isOperator(postfix[i])) {
+				exp.push((TreeNode) (postfix[i]));
+			} else {
+				TreeNode operator = new TreeNode(postfix[i]);
+				operator.setRight(exp.pop());
+				operator.setLeft(exp.pop());
 			}
+
 		}
 
-		return new ExpressionTree();
+		return operator;
 	}
 
-	public int evalTree(TreeNode root) {
+	private int evalTree(TreeNode root) {
 		TreeNode a = traverseTree(root);
 		TreeNode b = traverseTree(root);
 		return evalTree(a) * evalTree(b);
-
 	}
 
-	public String toPrefixNotation(TreeNode root) {
+	private String toPrefixNotation(TreeNode root) {
 		// traverse tree and returns root
 		TreeNode finalRoot = root;
 		// Base case: root == null, the tree is empty, do nothing
@@ -40,60 +45,57 @@ public class ExpressionTree {
 		return finalRoot.toString(); // this isnt' right!
 	}
 
-	public String toInfixNotation(TreeNode root) {
+	private String toInfixNotation(TreeNode root) {
 		// traverse tree and returns root
 		String finalRoot = root;
 		// Base case: root == null, the tree is empty, do nothing
 		if (root != null) {
 			finalRoot = toInfixNotation(root.getLeft());
-			process(root.getValue());
+			performOperation(root.getValue());
 			toInfixNotation(root.getRight());
 		}
 		return finalRoot.toString(); // haha what
 	}
 
-	public String toPostfixNotation(TreeNode root) {
+	private String toPostfixNotation(TreeNode root) {
 		// traverse tree and returns root
 		TreeNode finalRoot = root;
 		// Base case: root == null, the tree is empty, do nothing
 		if (root != null) {
 			toPostfixNotation(root.getLeft());
 			toPostfixNotation(root.getRight());
-			process(root);
+			performOperation(root.getValue());
 		}
 		return finalRoot.toString(); // look at thsi again lol
 	}
 
-	public int postfixEval(String[] postfix) {
-		Stack<String> exp = new Stack<String>();
+	private int postfixEval(String[] postfix) {
+		Stack<Integer> exp = new Stack<Integer>();
 		for (int i = 0; i < postfix.length; i++) {
-			if (isOperator(postfix[i])) {
+			if (!isOperator(postfix[i])) {
 				exp.push(postfix[i]);
 			} else
-				exp.pop();
-			//call performOperation at some point
+				exp.push(performOperation(exp.pop(), exp.pop(), postfix[i]));
 		}
+		return exp.pop();
 	}
 
-	public TreeNode traverseTree(TreeNode root) {
+	}
+
+	private TreeNode traverseTree(TreeNode root) { //this needs help
 		// traverse tree and returns root
 		TreeNode finalRoot = root;
 		// Base case: root == null, the tree is empty, do nothing
 		if (root != null) {
-			finalRoot = process(root.getValue());
+			finalRoot = performOperation(root.getValue());
 			traverseTree(root.getLeft());
 			traverseTree(root.getRight());
 		}
 		return finalRoot;
 	}
 
-	public boolean isOperator(String s) {
+	private boolean isOperator(String s) {
 		return (s.equals("+") || s.equals("*"));
-	}
-
-	private TreeNode process(Object n) {
-		// what the heck is this? check book boi
-		return new TreeNode(n);
 	}
 
 	private int performOperation(int a, int b, String operator) {
