be3eca937131afb4e7228211c61262585eb3e10e
gabbley <baniquedg@bishopireton.org>
2018-03-08 12:53:51 -0500

    *quietly* "Excuse me mrs kelly may i go to the bathroom"

--- Binary Trees/src/ExpressionTree.java
+++ Binary Trees/src/ExpressionTree.java
@@ -9,28 +9,30 @@ public class ExpressionTree {
 		super();
 	}
 
-	private TreeNode buildTree(String[] postfix) {
+	private TreeNode buildTree(String[] postfix) { // returns a treenode does
+													// that count as a tree
 
 		Stack<TreeNode> exp = new Stack<TreeNode>();
 		ExpressionTree tree = new ExpressionTree();
 		for (int i = 0; i < postfix.length; i++) {
 			if (!isOperator(postfix[i])) {
-				exp.push((TreeNode) (postfix[i]));
+				exp.push(new TreeNode(postfix[i]));
 			} else {
 				TreeNode operator = new TreeNode(postfix[i]);
 				operator.setRight(exp.pop());
 				operator.setLeft(exp.pop());
 			}
-
 		}
 
 		return exp.pop();
 	}
 
 	private int evalTree(TreeNode root) {
-		TreeNode a = traverseTree(root);
+		String operator = "";
+		TreeNode a = traverseTree(root); 
 		TreeNode b = traverseTree(root);
-		return evalTree(a) * evalTree(b);
+		ifOperator(operator);
+		return performOperation(evalTree(a), evalTree(b), operator);
 	}
 
 	private String toPrefixNotation(TreeNode root) {
@@ -38,7 +40,7 @@ public class ExpressionTree {
 		TreeNode finalRoot = root;
 		// Base case: root == null, the tree is empty, do nothing
 		if (root != null) {
-			finalRoot = process(root.getValue());
+			finalRoot = performOperation(root.getValue());
 			toPrefixNotation(root.getLeft());
 			toPrefixNotation(root.getRight());
 		}
@@ -51,7 +53,7 @@ public class ExpressionTree {
 		// Base case: root == null, the tree is empty, do nothing
 		if (root != null) {
 			finalRoot = toInfixNotation(root.getLeft());
-			performOperation(root.getValue());
+			performOperation(root.getValue()); // not right
 			toInfixNotation(root.getRight());
 		}
 		return finalRoot.toString(); // haha what
@@ -73,26 +75,13 @@ public class ExpressionTree {
 		Stack<Integer> exp = new Stack<Integer>();
 		for (int i = 0; i < postfix.length; i++) {
 			if (!isOperator(postfix[i])) {
-				exp.push(postfix[i]);
+				exp.push(Integer.parseInt(postfix[i]));
 			} else
 				exp.push(performOperation(exp.pop(), exp.pop(), postfix[i]));
 		}
 		return exp.pop();
 	}
 
-	}
-
-	private TreeNode traverseTree(TreeNode root) { //this needs help
-		// traverse tree and returns root
-		TreeNode finalRoot = root;
-		// Base case: root == null, the tree is empty, do nothing
-		if (root != null) {
-			finalRoot = performOperation(root.getValue());
-			traverseTree(root.getLeft());
-			traverseTree(root.getRight());
-		}
-		return finalRoot;
-	}
 
 	private boolean isOperator(String s) {
 		return (s.equals("+") || s.equals("*"));
@@ -104,6 +93,8 @@ public class ExpressionTree {
 			return a * b;
 		case "+":
 			return a + b;
+		default:
+			throw new IllegalArgumentException();
 		}
 
 	}
