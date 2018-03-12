cda2ed254f3089a2de04ec7693248c03dd672b9c
gabbley <baniquedg@bishopireton.org>
2018-03-12 16:08:45 -0400

    Added Javadocs  boi

--- Binary Trees/src/ExpressionTree.java
+++ Binary Trees/src/ExpressionTree.java
@@ -12,6 +12,16 @@ public class ExpressionTree {
 		super();
 	}
 
+
+	/**
+	 * Builds Expression tree from postfix expression
+	 * 
+	 * @param postfix
+	 * 	array of postfix expression
+	 * 
+	 * @return TreeNode
+	 * 	built ExpressionTree
+	 */
 	public TreeNode buildTree(String[] postfix) {
 
 		Stack<TreeNode> exp = new Stack<TreeNode>();
@@ -28,6 +38,16 @@ public class ExpressionTree {
 		return exp.pop();
 	}
 
+
+	/**
+	 * Traverses through a tree and evaluates
+	 * 
+	 * @param root
+	 * 	Tree to be evaluated
+	 * 
+	 * @return int
+	 * 	final value of the tree
+	 */
 	public int evalTree(TreeNode root) {
 		int evalNum = 0;
 		while (root != null) {
@@ -41,6 +61,16 @@ public class ExpressionTree {
 
 	}
 
+
+	/**
+	 * Converts ExpressionTree to prefix notation
+	 * 
+	 * @param root
+	 * 	ExpressionTree to be converted
+	 * 
+	 * @return String
+	 * 	Tree in prefix notation
+	 */
 	public String toPrefixNotation(TreeNode root) {
 		String notation = "";
 		if (root != null) {
@@ -51,6 +81,15 @@ public class ExpressionTree {
 		return notation;
 	}
 
+	/**
+	 * Converts ExpressionTree to infix notation
+	 * 
+	 * @param root
+	 * 	ExpressionTree to be converted
+	 * 
+	 * @return String
+	 * 	Tree in infix notation
+	 */
 	public String toInfixNotation(TreeNode root) {
 		String notation = "";
 		if (root != null) {
@@ -60,7 +99,16 @@ public class ExpressionTree {
 		}
 		return notation;
 	}
-
+	
+	/**
+	 * Converts ExpressionTree to postfix notation
+	 * 
+	 * @param root
+	 * 	ExpressionTree to be converted
+	 * 
+	 * @return String
+	 * 	Tree in postfix notation
+	 */
 	public String toPostfixNotation(TreeNode root) {
 		String notation = "";
 		if (root != null) {
@@ -71,6 +119,15 @@ public class ExpressionTree {
 		return notation;
 	}
 
+	/**
+	 * Evaluates a postfix expression
+	 * 
+	 * @param postfix
+	 * 	array of postfix expression
+	 * 
+	 * @return int
+	 * 	final value of postfix expression
+	 */
 	public int postfixEval(String[] postfix) {
 		Stack<Integer> exp = new Stack<Integer>();
 		for (int i = 0; i < postfix.length; i++) {
@@ -86,10 +143,34 @@ public class ExpressionTree {
 		return test;
 	}
 
+	/**
+	 * Determines if String sent in is an operator
+	 * 
+	 * @param s
+	 * 	String, integer or an operator (+ or * only)
+	 * 
+	 * @return boolean
+	 * 	true or false if an operator
+	 */
 	private boolean isOperator(String s) {
 		return (s.equals(ADDSIGN) || s.equals(MULTSIGN));
 	}
 
+	/**
+	 * Performs specified operation
+	 * 
+	 * @param a
+	 * 	first value to be added or multiplied
+	 * 
+	 * @param b
+	 * 	second value to be added or multiplied
+	 * 
+	 * @operator
+	 * 	operator to specify operation
+	 * 
+	 * @return int
+	 * 	value of operation performed
+	 */
 	private int performOperation(int a, int b, String operator) {
 		switch (operator) {
 		case MULTSIGN:
